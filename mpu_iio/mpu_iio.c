ee8bf1f6e3025678cbc1785ed1b36bed3fd82968
Israel Cepeda <israel.a.cepeda.lopez@intel.com>
2016-10-21 12:22:22 -0700

    mpu: mpu_iio: fix copyright
    Fixing copyright and adding author
    
    Signed-off-by: Israel Cepeda <israel.a.cepeda.lopez@intel.com>

--- mpu_iio/mpu_iio.c
+++ mpu_iio/mpu_iio.c
@@ -1,10 +1,12 @@
 /* MPU IIO Basic app
  *
- *  Copyright (c) 2016 Israel Cepeda
+ *  Copyright (c) 2016 Intel Corporation
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
  *  by the Free Software Foundation.
+ *
+ *  Author: Israel Cepeda <israel.a.cepeda.lopez@intel.com>
  */
 
 #include <stdio.h>
